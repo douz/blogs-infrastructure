2f6c1264a49f20193b8036cd4b21cd45116a96b6
Douglas Barahona <douglas.barahona@get10up.com>
2025-03-09 23:42:34 -0600

    Upgrade Kubernetes from version 1.31.1-do.3 to version 1.31.6-do.0 Add a third Kubernetes node

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ terraform {
 resource "digitalocean_kubernetes_cluster" "wp-blogs" {
   name    = "wp-blogs"
   region  = "nyc1"
-  version = "1.31.1-do.3"
+  version = "1.31.6-do.0"
 
   node_pool {
     name       = "wp-blogs-nodes"
     size       = "s-1vcpu-2gb"
-    node_count = 2
+    node_count = 3
   }
 }

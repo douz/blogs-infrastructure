5f0e3841c4e0448f026496ab1d4d95ae60d73c83
Douglas Barahona <douglas.barahona@get10up.com>
2025-12-07 02:57:00 -0600

    Decommission new K8S cluster

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,38 +9,3 @@ output "kubeconfig" {
   value     = digitalocean_kubernetes_cluster.wp-blogs.kube_config.0.raw_config
   sensitive = true
 }
-
-output "cluster_endpoint" {
-  description = "Endpoint of the DOKS Kubernetes API server"
-  value       = digitalocean_kubernetes_cluster.wp_sites_cluster.endpoint
-}
-
-output "cluster_name" {
-  description = "Name of the DOKS cluster"
-  value       = digitalocean_kubernetes_cluster.wp_sites_cluster.name
-}
-
-output "vpc_id" {
-  description = "UUID of the DigitalOcean VPC"
-  value       = digitalocean_vpc.wp-sites-vpc.id
-}
-
-output "db_private_ip" {
-  description = "Private IPv4 address of the MariaDB droplet"
-  value       = digitalocean_droplet.wp_sites_db.ipv4_address_private
-}
-
-output "redis_private_ip" {
-  description = "Private IPv4 address of the Redis droplet"
-  value       = digitalocean_droplet.redis.ipv4_address_private
-}
-
-output "db_droplet_id" {
-  description = "Droplet ID of the MariaDB server"
-  value       = digitalocean_droplet.wp_sites_db.id
-}
-
-output "redis_droplet_id" {
-  description = "Droplet ID of the Redis server"
-  value       = digitalocean_droplet.redis.id
-}
